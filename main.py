2ae36dd4973f81e6511cc0b6986d4cb2e2915eaa
pri2darshini <pri2darshini@gmail.com>
2022-09-16 11:05:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+def main():
+    #ask user for input
+    name =  input("What is your name?")
+    #print output for user
+    print(f"Hello {name}!")
+    return 0
+if __name__== "__main__":
+    sys.exit(main())
